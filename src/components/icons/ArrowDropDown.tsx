31d40921c16c7890073dc3861ef50ec70628deaf
EC <161985722+ec-bikes@users.noreply.github.com>
2024-03-13 03:09:50 -0700

    icon notes

--- src/components/icons/ArrowDropDown.tsx
+++ src/components/icons/ArrowDropDown.tsx
@@ -1,5 +1,7 @@
+// This icon is copied from @mui/icons-material because icon packages are huge and slow.
+// Icon search: https://mui.com/material-ui/material-icons/
+// This icon: https://github.com/mui/material-ui/blob/master/packages/mui-icons-material/lib/esm/ArrowDropDown.js
 import { createSvgIcon } from '@mui/material/utils';
 
-// copied from https://github.com/mui/material-ui/blob/master/packages/mui-icons-material/lib/esm/ArrowDropDown.js
 const ArrowDropDown = createSvgIcon(<path d="m7 10 5 5 5-5z" />, 'ArrowDropDown');
 export default ArrowDropDown;
