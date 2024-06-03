abd98c1b96aff491f1218104246fc33a82e9de4c
EC <161985722+ec-bikes@users.noreply.github.com>
2024-06-03 14:08:50 -0700

    WHY

--- pages/index/+Page.ts
+++ pages/index/+Page.ts
@@ -1,4 +1,12 @@
+import { useEffect } from 'react';
+import { navigate } from 'vike/client/router';
+import { baseUrl } from '../../common/constants.js';
+
 // Forces vike to generate an index page for github pages...
+// Also work around the fact the +guard redirect doesn't work on github pages??
 export function Page() {
+  useEffect(() => {
+    navigate(baseUrl + 'women');
+  }, []);
   return null;
 }
